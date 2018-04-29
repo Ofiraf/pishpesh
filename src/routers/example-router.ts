cecd8d625d038d8a228d125ee55cc24755f32068
Ofir Afargan <ofiraf@gmail.com>
2018-04-29 15:37:23 +0300

    router updated

--- src/routers/example-router.ts
+++ src/routers/example-router.ts
@@ -5,10 +5,18 @@ export class ExampleRouter {
     static routes(): Router {
         const router = Router();
 
-        router
-            .get('/:name', (request: Request, response: Response, next: NextFunction) => {
+        router.get('/:name', (request: Request, response: Response, next: NextFunction) => {
                 response.send(ExampleService.exampleServiceMethod(request.params.name));
-            });
+        });
+
+        router.post('/:name', (request: Request, response: Response, next: NextFunction) => {
+            response.send(ExampleService.exampleServiceMethod(request.params.name));
+        });
+
+        router.route('/:name/:id')
+        .get((request: Request, response: Response, next: NextFunction)=>{
+            response.send(`Oshri you are pesh ${request.params.name} ${request.params.id}` )
+        })
 
         return router;
     }
